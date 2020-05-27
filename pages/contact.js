84e652d5def1eeaadff97ff4636056efb937a5b5
Aakash Goel <goelaakash79@gmail.com>
2020-05-28 04:50:17 +0530

    contact section added

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import JoinSection from "../components/JoinSection";
+import Footer from "../components/Footer";
+
+const About = () => (
+	<Layout>
+		<div className="container">
+			<JoinSection />
+		</div>
+	</Layout>
+);
+
+export default About;
