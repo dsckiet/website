3a92f4aa299cce84efd846b5bda4eef6d753a5f1
Rohan <rhnmht30@gmail.com>
2020-05-30 23:27:10 +0530

    fix: service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../components/main.css";
+import { register, unregister } from "next-offline/runtime";
+import React from "react";
 
-export default function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+export default class MyApp extends React.Component {
+	componentDidMount() {
+		register();
+	}
+	componentWillUnmount() {
+		unregister();
+	}
+
+	render() {
+		const { Component, pageProps } = this.props;
+		return <Component {...pageProps} />;
+	}
 }
